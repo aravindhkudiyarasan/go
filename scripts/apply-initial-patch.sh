78072a51177dbd8fa403fe1459a3c767e367fdb6
K Aravindh <karavindh@zscaler.com>
2025-04-04 05:15:13 +0530

    ---

--- scripts/apply-initial-patch.sh
+++ scripts/apply-initial-patch.sh
@@ -7,6 +7,6 @@ set -ex
 git apply -v ../patches/000-initial-setup.patch
 # Add the initial changes to the index so the later diff ignores them.
 git add .
-git commit -m phase1
 git config --local user.email "karavindh@zscaler.com"
 git config --local user.name "karavindh"
+git commit -m phase1
